c645096f6dce69bf7f4f52836bddce286edda55b
Chansik Yun <chansigi@gmail.com>
2022-05-11 11:36:25 +0900

    .

--- 9_함수3.c
+++ 9_함수3.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-void print_array(int* arr, int n);
+void print_array(int* arr, int n)
 {
     // int lenght = sizeof(arr) / sizeof(arr[0]);
-    printf("print_array: %d\n", sizeof(arr));
+    printf("print_array: %lu\n", sizeof(arr));
 }
 
 int main(void)
 {
     int arr[10];
-    printf("main: %d\n", sizeof(arr));
+    printf("main: %lu\n", sizeof(arr));
 
     print_array(arr, 10);
     // Decay: print_array(&arr[0]);
