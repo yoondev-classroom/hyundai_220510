b61adaa1fd07edaa206649a4a77938390273af26
Chansik Yun <chansigi@gmail.com>
2022-05-13 14:30:38 +0900

    .

---
+++ 19_구조체3.c
@@ -0,0 +1,36 @@
+// 19_구조체3.c
+
+#include <stdio.h>
+
+// 구조체 태그명: point
+//  구조체 타입: struct point
+typedef struct point {
+    // 구조체 멤버 변수
+    int x;
+    int y;
+} POINT;
+
+int main(void)
+{
+    // POINT pt1 = { 10, 20 };
+    // C99 - 멤버 지정 초기화
+    POINT pt1 = { .x = 10, .y = 20 };
+
+    printf("%d %d\n", pt1.x, pt1.y);
+
+    return 0;
+}
+
+#if 0
+int main(void)
+{
+    POINT pt1;
+    // .(dot) 연산자: 구조체 멤버 변수에 접근이 가능합니다.
+    pt1.x = 10;
+    pt1.y = 20;
+
+    printf("%d %d\n", pt1.x, pt1.y);
+
+    return 0;
+}
+#endif
