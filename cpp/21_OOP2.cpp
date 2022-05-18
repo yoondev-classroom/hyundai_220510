bf895414daa3d24e3fdf983d279be72241af8577
Chansik Yun <chansigi@gmail.com>
2022-05-18 13:49:19 +0900

    .

--- cpp/21_OOP2.cpp
+++ cpp/21_OOP2.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 // 2) 스택 타입을 설계합니다.
 // => 사용자 정의 타입
+// 문제점
+// - 스택을 조작하는 모든 함수가,
+//   반드시 스택을 인자로 전달받아야 합니다.
+// 원인: 상태를 관리하는 구조체의 멤버 데이터 변수와
+//      상태를 조작하는 함수가 일반 함수로 되어 있기
+//      때문입니다.
 
 struct Stack {
     int buff[10];
@@ -25,8 +31,18 @@ int pop(Stack* s)
     return s->buff[--s->top];
 }
 
+Stack s2;
+
 int main()
 {
+    init(&s2);
+    push(&s2, 100);
+    push(&s2, 200);
+    push(&s2, 300);
+    cout << pop(&s2) << endl;
+    cout << pop(&s2) << endl;
+    cout << pop(&s2) << endl;
+
     Stack s1;
     init(&s1);
 
---
+++ cpp/21_OOP3.cpp
@@ -0,0 +1,61 @@
+// 21_OOP3.cpp
+#include <iostream>
+using namespace std;
+
+// 2) 상태를 관리하는 멤버 데이터와 상태를 조작하는 함수를
+//    묶기.
+//  - 객체(상태 + 행위)
+//  - OOP(Object Oriented Programming) : 객체 지향 프로그래밍
+//   : 정보 은닉
+//     외부에서 객체의 상태를 함부로 변경할 수 없도록 하는 언어적 기능
+//     => 접근 지정자
+//     - private: 내부에서만 접근이 가능합니다.
+//     - public: 외부에서도 접근이 가능합니다.
+//     => struct는 기본 접근 지정자가 public 입니다.
+//        class는 기본 접근 지정자가 private 입니다.
+//
+//     class는 객체를 만들기 위한 틀
+
+class Stack {
+private:
+    // 멤버 데이터 변수
+    int buff[10];
+    int top;
+
+public:
+    // 멤버 함수 / 메소드
+    void init()
+    {
+        top = 0;
+    }
+
+    void push(int n)
+    {
+        buff[top++] = n;
+    }
+
+    int pop()
+    {
+        return buff[--top];
+    }
+};
+
+Stack s2;
+
+int main()
+{
+    Stack s2;
+    s2.init();
+    // s2.top = -1;
+
+    Stack s1; // 객체
+    s1.init();
+
+    s1.push(10);
+    s1.push(20);
+    s1.push(30);
+
+    cout << s1.pop() << endl;
+    cout << s1.pop() << endl;
+    cout << s1.pop() << endl;
+}
