5592773f43cb474999b2c326b6734355c60480f6
Chansik Yun <chansigi@gmail.com>
2022-05-23 11:55:42 +0900

    .

--- cpp/3_iostream.cpp
+++ cpp/3_iostream.cpp
@@ -13,13 +13,16 @@ using namespace std;
 // - 표준 에러: cerr
 //------------------------------
 // 1. n의 주소를 전달하지 않아도, n의 값이 변경됩니다.
+//  => 레퍼런스
+
 // 2. 서식이 존재하지 않아도, 각 타입에 맞는 값이 입력됩니다.
 //  => 함수 오버로딩
 
-// 3. std::cout
-//    std::cin
-//    std::endl
+// 3. std::cout => ostream
+//    std::cin  => istream
+//    std::endl => ostream& endl(ostream&) {} 함수
 //    정체는 무엇인가?
+//    "연산자 오버로딩을 통해 기능이 제공되고 있다."
 
 int main()
 {
